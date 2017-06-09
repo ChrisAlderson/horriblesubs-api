276be8e3859a9dca9a2063bbbaa9a1b2a0ce5a9d
Chris Alderson <chrisalderson@protonmail.com>
2017-06-09 16:52:18 +0200

    destruct assert

--- test/horriblesub.js
+++ test/horriblesub.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const chai = require('chai');
-const assert = chai.assert;
+const { assert } = require('chai');
 const HorribleSubsAPI = require('../horriblesubs-api');
 
 describe('HorribleSubs', () => {
